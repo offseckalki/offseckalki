5828ae6a585ed4b64045090db43b00be4104b141
Drash Tyagi <61248381+offseckalki@users.noreply.github.com>
2022-12-14 18:47:24 +0530

    Create kalki.js

---
+++ kalki.js
@@ -0,0 +1,7 @@
+<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="HORIZONTAL" data-vanity="offseckalki" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/offseckalki?trk=profile-badge">Drash Tyagi</a></div>
+              
+
+
+
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript">
+  </script>
